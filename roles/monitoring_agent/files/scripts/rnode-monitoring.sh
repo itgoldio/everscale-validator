075895536b5cf7e1596e14585dcb50edc92f1f53
Ilya Vasilyev <ilya.vasilev@itglobal.com>
2021-12-22 18:22:23 +0300

    monitoring: rnode-monitoring.sh

--- roles/monitoring_agent/files/scripts/rnode-monitoring.sh
+++ roles/monitoring_agent/files/scripts/rnode-monitoring.sh
@@ -53,97 +53,9 @@ usage() {
   exit $STATE_UNKNOWN
 }
 
-getElectorAddr(){
-  $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c 'getconfig 1' | grep -iq error
-  if [[ $? -ne 0 ]]
-  then
-    myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getconfig 1" | jq -r '.p1' )
-    [[ -n $myRes ]] echo "-1:${myRes}" || echo ""
-  fi
-}
-
-getAccountBalance() {
-  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getaccount $VALIDATOR_WALLET_ADDR" | jq -r ".balance" )
-  [[ -n $myRes ]] && echo $myRes || echo ""
-}
-
-isValidatingNow() {
-  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".in_current_vset_p34" )
-  [[ -n $myRes ]] && echo $myRes || echo ""
-}
-
-isValidatingNext() {
-utime=$( date +%s )
-TON_CONFIG_34=$( $TON_CONSOLE -C $TON_CONSOLE_CONFIG -c "getconfig 34" )
-TON_CONFIG_34_JSON=$( echo $TON_CONFIG_34 | awk '{split($0, a, "param:"); print a[2]}' )
-TON_CONFIG_15=$( $TON_CONSOLE -C $TON_CONSOLE_CONFIG -c "getconfig 15" )
-TON_CONFIG_15_JSON=$( echo $TON_CONFIG_15 | awk '{split($0, a, "param:"); print a[2]}' )
-TON_CURRENT_VALIDATION_END=$( echo $TON_CONFIG_34_JSON | jq '.p34.utime_until') 
-TON_ELECTIONS_START_BEFORE=$( echo $TON_CONFIG_15_JSON | jq '.p15.elections_start_before' )
-TON_ELECTIONS_END_BEFORE=$( echo $TON_CONFIG_15_JSON | jq '.p15.elections_end_before' )
-
-# в этот период проверяем
-TON_CURRENT_VALIDATION_END=$( echo "${TON_CONFIG_34_JSON}" | jq '.p34.utime_until' )
-TON_ELECTIONS_START_BEFORE=$( echo "${TON_CONFIG_15_JSON}" | jq '.p15.elections_start_before' )
-
-if [[ -z $TON_CURRENT_VALIDATION_END || -z $TON_ELECTIONS_START_BEFORE ]]
-then
-  echo -e "UNKNOWN - no data in .p34.utime_until or .p15.elections_start_before | validatingNext=-1;;;;"
-  exit $STATE_UNKNOWN
-fi
-
-TON_ELECTIONS_START=$(($TON_CURRENT_VALIDATION_END - $TON_ELECTIONS_START_BEFORE))
-
-if [[ $utime -ge $TON_ELECTIONS_START && $utime -le $TON_CURRENT_VALIDATION_END ]]
-then
-  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".in_next_vset_p36" )
-  if [[ "$myRes" == "true" ]]
-  then
-    echo "OK - Node will validate next round: 1 | validatingNext=1;;;;"
-    exit $STATE_OK
-  else
-    echo "CRITICAL - Node will validate next round: 0 | validatingNext=0;;;;"
-    exit $STATE_CRITICAL
-  fi
-else
-  echo -e "UNKNOWN - Elections not defined yet | validatingNext=-1;;;;"
-  exit $STATE_OK
-fi
-}
-
-getDePollBalance() {
+getDePoolBalance() {
   myRes=$( $TON_CLI -j -c $TON_CLI_CONFIG account $DEPOOL_ADDR | jq -r ".balance" )
   [[ -n $myRes ]] && echo $myRes || echo ""
-}
-
-
-getTimeDiff() {
-# timeDiff  - using script ./ton-node-diff.sh
-#   returning values from script: integer
-#   checking values and returning code by check
-#     1 sec - 600 sec  - normal -> return 0
-#     600 sec - 1199 sec - warning -> return 1
-#     1200 sec > - critical -> return 2
-#     0 < - unknown -> unknown 3
-  warnValue=${1}
-  critValue=${2}
-  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".timediff" )
-  if [[ "$myRes" -lt "$warnValue" ]]
-  then
-    echo "OK - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
-    exit $STATE_OK
-  elif [[ "$myRes" -ge "$warnValue" && "$myRes" -lt "$critValue" ]]
-  then
-    echo "WARNING - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
-    exit $STATE_WARNING
-  elif [[ "$myRes" -ge "$critValue" ]]
-  then
-    echo "CRITICAL - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
-    exit $STATE_CRITICAL
-  else
-    echo "UNKNOWN - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
-    exit $STATE_UNKNOWN
-  fi
 }
 
 checkStatus() {
@@ -166,7 +78,7 @@ checkStatus() {
     
   case "$type" in
     walletBalance ) myMsg="Node wallet balance:" ;;
-    dePoolBalance ) myMsg="DePool balance:" ;;
+    dePoolBalance ) curValue=$( getDePoolBalance ) ; myMsg="DePool balance:" ;;
     proxy1balance ) myMsg="DePool proxy 1 balance:" ;;
     proxy2balance ) myMsg="DePool proxy 2 balance:" ;;
     tickTokBalance ) myMsg="TickTok balance:" ;;
@@ -200,7 +112,85 @@ checkStatus() {
   fi
 }
 
+getElectorAddr(){
+  $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c 'getconfig 1' | grep -iq error
+  if [[ $? -ne 0 ]]
+  then
+    myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getconfig 1" | jq -r '.p1' )
+    [[ -n $myRes ]] && echo "-1:${myRes}" || echo ""
+  fi
+}
 
+getAccountBalance() {
+  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getaccount $VALIDATOR_WALLET_ADDR" | jq -r ".balance" )
+  [[ -n $myRes ]] && echo $myRes || echo ""
+}
+
+isValidatingNow() {
+  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".in_current_vset_p34" )
+  [[ -n $myRes ]] && echo $myRes || echo ""
+}
+
+isValidatingNext() {
+utime=$( date +%s )
+
+# в этот период проверяем
+TON_CURRENT_VALIDATION_END=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getconfig 34"  | jq -r ".p34.utime_until" )
+TON_ELECTIONS_START_BEFORE=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getconfig 15"  | jq -r ".p15.elections_start_before" )
+
+if [[ -z $TON_CURRENT_VALIDATION_END || -z $TON_ELECTIONS_START_BEFORE ]]
+then
+  echo -e "UNKNOWN - no data in .p34.utime_until or .p15.elections_start_before | validatingNext=-1;;;;"
+  exit $STATE_UNKNOWN
+fi
+
+TON_ELECTIONS_START=$(($TON_CURRENT_VALIDATION_END - $TON_ELECTIONS_START_BEFORE))
+
+if [[ $utime -ge $TON_ELECTIONS_START && $utime -le $TON_CURRENT_VALIDATION_END ]]
+then
+  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".in_next_vset_p36" )
+  if [[ "$myRes" == "true" ]]
+  then
+    echo "OK - Node will validate next round: 1 | validatingNext=1;;;;"
+    exit $STATE_OK
+  else
+    echo "CRITICAL - Node will validate next round: 0 | validatingNext=0;;;;"
+    exit $STATE_CRITICAL
+  fi
+else
+  echo -e "UNKNOWN - Elections not defined yet | validatingNext=-1;;;;"
+  exit $STATE_OK
+fi
+}
+
+getTimeDiff() {
+# timeDiff  - using script ./ton-node-diff.sh
+#   returning values from script: integer
+#   checking values and returning code by check
+#     1 sec - 600 sec  - normal -> return 0
+#     600 sec - 1199 sec - warning -> return 1
+#     1200 sec > - critical -> return 2
+#     0 < - unknown -> unknown 3
+  warnValue=${1}
+  critValue=${2}
+  myRes=$( $TON_CONSOLE -j -C $TON_CONSOLE_CONFIG -c "getstats" | jq -r ".timediff" )
+  if [[ "$myRes" -lt "$warnValue" ]]
+  then
+    echo "OK - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
+    exit $STATE_OK
+  elif [[ "$myRes" -ge "$warnValue" && "$myRes" -lt "$critValue" ]]
+  then
+    echo "WARNING - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
+    exit $STATE_WARNING
+  elif [[ "$myRes" -ge "$critValue" ]]
+  then
+    echo "CRITICAL - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
+    exit $STATE_CRITICAL
+  else
+    echo "UNKNOWN - Node time diff: $myRes | timeDiff=$myRes;${warnValue};${critValue};;"
+    exit $STATE_UNKNOWN
+  fi
+}
 
 ### main script
 
@@ -234,11 +224,11 @@ then
   while true
   do
     case "$typeCheck" in
-      timeDiff ) timeDiff "${warnValue}" "${critValue}" ;;
+      timeDiff ) getTimeDiff "${warnValue}" "${critValue}" ;;
       walletBalance )
         checkStatus walletBalance "${warnValue}" "${critValue}" $( "${tonScriptsDir}""${walletBalanceScript}" ) ;;
       dePoolBalance )
-        checkStatus dePoolBalance "${warnValue}" "${critValue}" $( "${tonScriptsDir}""${depoolBalanceScript}" ) ;;
+        checkStatus dePoolBalance "${warnValue}" "${critValue}" ;;
       proxy1balance )
         checkStatus proxy1balance "${warnValue}" "${critValue}" $( "${tonScriptsDir}""${depoolproxy1BalanceScript}" 2>/dev/null ) ;;
       proxy2balance )
